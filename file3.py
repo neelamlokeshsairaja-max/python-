4b52a424269773c7dac09f4d6f485f131d0c46ba
neelamlokeshsairaja-max <neelamlokeshsairaja@gmail.com>
2026-09-15 15:46:30 +0530

    Add files via upload

---
+++ file3.py
@@ -0,0 +1,8 @@
+'''if else if : when we have multiple conditions to check then we can use if else if statement.'''
+marks = 87
+if marks >= 90:
+    print("grade a")
+elif marks >= 80:
+    print("grade b")
+else:
+    print("grade c")
